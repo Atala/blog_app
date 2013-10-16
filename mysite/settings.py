e2ba7eb167f320d21ce5b386e49618f32ba1303f
Guillope <alois.guillope@gmail.com>
2013-10-16 13:41:16 +0200

    Change to postgre

--- mysite/settings.py
+++ mysite/settings.py
@@ -10,6 +10,10 @@ ADMINS = (
 MANAGERS = ADMINS
 
 
+# Parse database configuration from $DATABASE_URL
+import dj_database_url
+DATABASES['default'] =  dj_database_url.config()
+
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
 ALLOWED_HOSTS = []
@@ -148,9 +152,6 @@ LOGGING = {
     }
 }
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
 
 # Honor the 'X-Forwarded-Proto' header for request.is_secure()
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
