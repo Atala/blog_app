d6b2589a274d32aba01dc9296f733be4bcc5fad3
Guillope <alois.guillope@gmail.com>
2013-10-16 12:26:41 +0200

    correct foreign key

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -22,7 +22,7 @@ class Entry(models.Model):
 	slug = models.SlugField(max_length=100, unique=True)
 	body = models.TextField()
 	posted  = models.DateField(db_index=True, auto_now_add=True)
-	category = models.ForeignKey('blog.category')
+	category = models.ForeignKey(Category)
 
 	def __unicode__ (self):
 		return str(self.title)
@@ -45,7 +45,7 @@ class Author(models.Model):
 
 class Book(models.Model):
 	title = models.TextField()
-	author = models.ManyToManyField('blog.author')
+	author = models.ManyToManyField(Author)
 	
 	
 	def __unicode__ (self):
